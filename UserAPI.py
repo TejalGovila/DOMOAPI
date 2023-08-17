c7ea8188a13d11e3f84c6517da8ccbbe650981ed
TejalGovila <47816254+TejalGovila@users.noreply.github.com>
2023-08-17 13:48:49 +0530

    Update UserAPI.py

--- UserAPI.py
+++ UserAPI.py
@@ -1,1 +1,1 @@
-
+curl -v -u 8b0abc02-99db-4a43-82aa-41dc06d519b5:3566a763c3c81f39f1352ba53689db4b999c9edb4c34bdb2a6218a19826da3b1 "https://api.domo.com/oauth/token?grant_type=client_credentials&scope=user"
